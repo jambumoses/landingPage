bd8243535918592b9b5d6c5de8f814c24eb1ebf3
jambu moses <jambcareer@gmail.com>
2023-07-28 13:45:02 +0300

    last forgotten

--- src/App.js
+++ src/App.js
@@ -1,49 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import Nav from './components/Nav';
+import React, { useEffect, useState } from "react";
+import Nav from "./components/Nav";
 import "./components/statics/css/main.css";
-import $ from 'jquery';
-import { useDispatch, useSelector } from 'react-redux';
+import $ from "jquery";
+import { useDispatch, useSelector } from "react-redux";
 
 /* import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
  */
 
-import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from './components/Home'
-import About from "./components/About"
-import Brands from "./components/Brands"
-import Orders from "./components/Orders"
-import Categories from "./components/Categories"
-import Contact from "./components/Contact"
-import Accounts from "./components/Accounts"
-import Cart from "./components/Cart"
-import Footer from './components/Footer';
-import Register from './components/Register';
-import Login from './components/Login';
-import LostPassword from './components/LostPassword';
-import Verification from './components/Verification';
-import { constantActions } from './store/constantSlice';
-import { cartActions } from './store/CartSlice';
-import Dashboard from './components/Dash/Dashboard';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Brands from "./components/Brands";
+import Orders from "./components/Orders";
+import Categories from "./components/Categories";
+import Contact from "./components/Contact";
+import Accounts from "./components/Accounts";
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import LostPassword from "./components/LostPassword";
+import Verification from "./components/Verification";
+import { constantActions } from "./store/constantSlice";
+import { cartActions } from "./store/CartSlice";
+import Dashboard from "./components/Dash/Dashboard";
 
-import axios from 'axios';
-import { bannerActions } from './store/BannerSlice';
-import { BrandsActions } from './store/BrandsSlice';
-import { productActions } from './store/productSlice';
-import { categoriesActions } from './store/categoriesSlice';
-
-
+import axios from "axios";
+import { bannerActions } from "./store/BannerSlice";
+import { BrandsActions } from "./store/BrandsSlice";
+import { productActions } from "./store/productSlice";
+import { categoriesActions } from "./store/categoriesSlice";
 
 export default function App() {
-
   const dispatch = useDispatch();
 
-  const browserIcons = useSelector(state=>state.merchant.logo.browserTab)
-  const currentPage = useSelector(state=>state.constant.currentPage)
-  const PageTitles = useSelector(state=>state.constant.currentPageTitle)
+  const browserIcons = useSelector((state) => state.merchant.logo.browserTab);
+  const webDescription = useSelector((state) => state.constant.webDescription);
+  const webKeywords = useSelector((state) => state.constant.webKeywords);
+  const webAuthor = useSelector((state) => state.constant.webAuthor);
 
-/* 
+  const currentPage = useSelector((state) => state.constant.currentPage);
+  const PageTitles = useSelector((state) => state.constant.currentPageTitle);
+
+  /* 
   // banner information
   const homeBanner = axios.create({
     baseURL: "http://localhost:3500/arafat/api/banners/homebanner",
@@ -109,150 +110,139 @@ export default function App() {
       },
     }); */
 
-    
-
-
-  if(currentPage == 'home'){
-    $('body').css("overflow","hidden");
-  }else{
-    $('body').css({
-      "overflow-x":"hidden",
-      "overflow-y":"auto"
+  if (currentPage == "home") {
+    $("body").css("overflow", "hidden");
+  } else {
+    $("body").css({
+      "overflow-x": "hidden",
+      "overflow-y": "auto",
     });
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     /* Browser icons */
     $("#browser-icons").attr("href", browserIcons);
 
     // updating page titles
     $("#app-title").text(PageTitles);
+    
+    $("#web-description").attr("content",webDescription);
+    $("#web-keywords").attr("content",webKeywords);
+    $("#web-author").attr("content",webAuthor);
 
     // updating cart data
     dispatch(cartActions.updateCartCount());
 
     //updating home Banner from database
-/*     homeBanner.get('?_limit=10').then((response) => {
+    /*     homeBanner.get('?_limit=10').then((response) => {
       dispatch(bannerActions.getHomeBanner(response.data));
    }); */
 
-   //updating brands Banner from database
-/*    brandsBanner.get().then((response)=>{
+    //updating brands Banner from database
+    /*    brandsBanner.get().then((response)=>{
     dispatch(bannerActions.getBrandsBanner(response.data[0]))
    }); */
 
-   //updating contact Banner from database
-/*    contactBanner.get().then((response)=>{
+    //updating contact Banner from database
+    /*    contactBanner.get().then((response)=>{
     dispatch(bannerActions.getContactBanner(response.data[0]))
    }); */
 
-   //updating about banner
-/*    aboutBanner.get().then((response)=>{
+    //updating about banner
+    /*    aboutBanner.get().then((response)=>{
     dispatch(bannerActions.getAboutBanner(response.data[0]))
    }); */
 
-   // brands data
-/*    brands.get().then((response)=>{
+    // brands data
+    /*    brands.get().then((response)=>{
     dispatch(BrandsActions.getBrands(response.data))
    }); */
 
-  // products data
-/*    products.get().then((response)=>{
+    // products data
+    /*    products.get().then((response)=>{
     dispatch(productActions.getProducts(response.data))
    }); */
 
-  // topsells data
-/*     topsells.get().then((response)=>{
+    // topsells data
+    /*     topsells.get().then((response)=>{
     dispatch(productActions.getTrends(response.data))
   }); */
 
-  // categories data
-/*    categories.get().then((response)=>{
+    // categories data
+    /*    categories.get().then((response)=>{
     dispatch(categoriesActions.getCategories(response.data))
    }); */
-
-  })
-
+  });
 
   return (
     <>
-    <Router>
-        {
-          (currentPage != "Dashboard") &&
-          (currentPage != "DashMerchant") &&
-          (currentPage != "DashBanners") &&
-          (currentPage != "DashBrands") &&
-          (currentPage != "DashProducts") &&
-          (currentPage != "DashCategories") &&
-          (currentPage != "DashOrders") &&
-          (currentPage != "DashTrends") &&
-          (currentPage != "DashUsers") &&
-          (currentPage != "DashMessages") &&
-          (currentPage != "DashEmailListing") &&
-          (currentPage != "DashSettings") &&
-          <Nav/>
-        }
-      
+      <Router>
+        {currentPage != "Dashboard" &&
+          currentPage != "DashMerchant" &&
+          currentPage != "DashBanners" &&
+          currentPage != "DashBrands" &&
+          currentPage != "DashProducts" &&
+          currentPage != "DashCategories" &&
+          currentPage != "DashOrders" &&
+          currentPage != "DashTrends" &&
+          currentPage != "DashUsers" &&
+          currentPage != "DashMessages" &&
+          currentPage != "DashEmailListing" &&
+          currentPage != "DashSettings" && <Nav />}
+
         <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/home" component={Home}/>
-                <Route path="/index" component={Home}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/index" component={Home} />
 
-                <Route path="/about" component={About} />
-                <Route path="/brands" component={Brands} />
-                <Route path="/orders" component={Orders}/>
-                <Route path="/categories" component={Categories} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/accounts" component={Accounts} />
+          <Route path="/about" component={About} />
+          <Route path="/brands" component={Brands} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/categories" component={Categories} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/accounts" component={Accounts} />
 
-                {/* accounts */}
-                <Route path="/login" component={Login} />
-                <Route path="/lost-password" component={LostPassword}/>
-                <Route path="/register" component={Register} />
-                <Route path="/verify" component={Verification} />
-                <Route path="/cart" component={Cart} />
+          {/* accounts */}
+          <Route path="/login" component={Login} />
+          <Route path="/lost-password" component={LostPassword} />
+          <Route path="/register" component={Register} />
+          <Route path="/verify" component={Verification} />
+          <Route path="/cart" component={Cart} />
 
-                {/* Dashboard */}
-                <Route path="/dashboard" component={Dashboard} />
+          {/* Dashboard */}
+          <Route path="/dashboard" component={Dashboard} />
 
-                <Route path="/dashboard" component={Dashboard} />
-                <Route path="/dashboardmerchant" component={Dashboard} />
-                <Route path="/dashboardbanners" component={Dashboard} />
-                <Route path="/dashboardbrands" component={Dashboard} />
-                <Route path="/dashboardproducts" component={Dashboard} />
-                <Route path="/dashboardcategories" component={Dashboard} />
-                <Route path="/dashboardorders" component={Dashboard} />
-                <Route path="/dashboardtrends" component={Dashboard} />
-                <Route path="/dashboardusers" component={Dashboard} />
-                <Route path="/messages" component={Dashboard} />
-                <Route path="/dashboardemaillisting" component={Dashboard} />
-                <Route path="/dashboardsettings" component={Dashboard} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/dashboardmerchant" component={Dashboard} />
+          <Route path="/dashboardbanners" component={Dashboard} />
+          <Route path="/dashboardbrands" component={Dashboard} />
+          <Route path="/dashboardproducts" component={Dashboard} />
+          <Route path="/dashboardcategories" component={Dashboard} />
+          <Route path="/dashboardorders" component={Dashboard} />
+          <Route path="/dashboardtrends" component={Dashboard} />
+          <Route path="/dashboardusers" component={Dashboard} />
+          <Route path="/messages" component={Dashboard} />
+          <Route path="/dashboardemaillisting" component={Dashboard} />
+          <Route path="/dashboardsettings" component={Dashboard} />
 
-
-
-
-                {/* 404 */}
-                <Route path='*' component={Home}/>
+          {/* 404 */}
+          <Route path="*" component={Home} />
         </Switch>
 
-        {
-          (currentPage != "home") && 
-          (currentPage != "Dashboard") &&
-          (currentPage != "DashMerchant") &&
-          (currentPage != "DashBanners") &&
-          (currentPage != "DashBrands") &&
-          (currentPage != "DashProducts") &&
-          (currentPage != "DashCategories") &&
-          (currentPage != "DashOrders") &&
-          (currentPage != "DashTrends") &&
-          (currentPage != "DashUsers") &&
-          (currentPage != "DashMessages") &&
-          (currentPage != "DashEmailListing") &&
-          (currentPage != "DashSettings")&& 
-          <Footer/>
-        }
-        
-    </Router>
+        {currentPage != "home" &&
+          currentPage != "Dashboard" &&
+          currentPage != "DashMerchant" &&
+          currentPage != "DashBanners" &&
+          currentPage != "DashBrands" &&
+          currentPage != "DashProducts" &&
+          currentPage != "DashCategories" &&
+          currentPage != "DashOrders" &&
+          currentPage != "DashTrends" &&
+          currentPage != "DashUsers" &&
+          currentPage != "DashMessages" &&
+          currentPage != "DashEmailListing" &&
+          currentPage != "DashSettings" && <Footer />}
+      </Router>
     </>
-  )
+  );
 }
--- src/store/constantSlice.js
+++ src/store/constantSlice.js
@@ -1,25 +1,28 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import CONSTANTS from "../components/lib/ConstactInformation";
 
 const constantSlice = createSlice({
-    name: "constant",
-    initialState: {
-        currentPageTitle: "",
-        currentPage: "",
-        data: CONSTANTS,
-
+  name: "constant",
+  initialState: {
+    currentPageTitle: "",
+    currentPage: "",
+    webDescription: "eudaimonia enterprises Distributor LTD. your reliable partner",
+    webKeywords:
+      "eudaimonia,eudaimonia.com, enterprises Distributor LTD,LTD, Distributor,enterprises",
+    webAuthor: "www.jambumoses.com",
+    data: CONSTANTS,
+  },
+  reducers: {
+    updateConstants(state, action) {
+      state.data = action.payload;
     },
-    reducers:{
-        updateConstants(state,action){
-            state.data = action.payload;
-        },
-        updatePageTitles(state,action){
-            state.currentPageTitle = action.payload;
-        },
-        setCurrentPage(state,action){
-            state.currentPage = action.payload;
-        },
-    }
+    updatePageTitles(state, action) {
+      state.currentPageTitle = action.payload;
+    },
+    setCurrentPage(state, action) {
+      state.currentPage = action.payload;
+    },
+  },
 });
 
 export const constantActions = constantSlice.actions;
